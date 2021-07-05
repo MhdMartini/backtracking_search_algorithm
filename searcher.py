d2c86c608109b97cde08360e7483919832c33e93
MhdMartini <mohamed_martini@student.uml.edu>
2021-07-05 15:08:32 -0400

    commented out return to base

--- searcher.py
+++ searcher.py
@@ -25,9 +25,9 @@ class Searcher:
         self.searcher(self.current)
 
         # return back to base - optional
-        final_path = ShortestPath(
-            map_=self.start_map, start=self.visited_coords[-1], destination=self.start).shortest_path
-        self.visited_coords.extend(final_path)
+        # final_path = ShortestPath(
+        #     map_=self.start_map, start=self.visited_coords[-1], destination=self.start).shortest_path
+        # self.visited_coords.extend(final_path)
 
         self.print_result()
         if self.vid:
